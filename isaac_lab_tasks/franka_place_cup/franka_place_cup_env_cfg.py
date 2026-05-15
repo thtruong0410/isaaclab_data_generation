a83c9d220766ee1191f58ec6075571ab45180989
thtruong0410 <thtruong0410@users.noreply.github.com>
2026-05-15 23:16:51 +0700

    Increase place cup box size

--- isaac_lab_tasks/franka_place_cup/franka_place_cup_env_cfg.py
+++ isaac_lab_tasks/franka_place_cup/franka_place_cup_env_cfg.py
@@ -198,7 +198,7 @@ class FrankaPlaceCupEnvCfg(FrankaCubeLiftEnvCfg):
             init_state=RigidObjectCfg.InitialStateCfg(pos=BOX_INIT_POS, rot=(1.0, 0.0, 0.0, 0.0)),
             spawn=UsdFileCfg(
                 usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Mimic/nut_pour_task/nut_pour_assets/sorting_bin_blue.usd",
-                scale=(0.75, 1.0, 2.2),
+                scale=(0.90, 1.20, 2.4),
                 rigid_props=sim_utils.RigidBodyPropertiesCfg(),
             ),
         )
